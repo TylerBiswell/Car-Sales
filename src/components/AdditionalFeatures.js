127aacedfe4f23b18651cccafe39f9db28970f3d
Tyler Biswell <tsbiswell@gmail.com>
2019-09-12 10:05:44 -0600

    add feature functionality

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -2,14 +2,24 @@ import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 import { connect } from 'react-redux';
 
+import { addFeature } from '../actions';
+
 const AdditionalFeatures = props => {
+  const handleClick = id => {
+    props.addFeature(id);
+  };
+
   return (
     <div className='content'>
       <h4>Additional Features</h4>
       {props.store.length ? (
         <ol type='1'>
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature
+              key={item.id}
+              feature={item}
+              handleClick={handleClick}
+            />
           ))}
         </ol>
       ) : (
@@ -27,5 +37,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {},
+  { addFeature },
 )(AdditionalFeatures);
